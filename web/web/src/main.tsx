ff0600d94440f740e047bb400efc6a986758e55b
Simon Kamau <simonkamau690@gmail.com>
2023-08-09 16:53:03 +0300

    Update Main.tsx

--- web/web/src/main.tsx
+++ web/web/src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
+import App from './App';
 
+const rootElement = document.getElementById('root');
 
-import './index.css'
-import App from './App.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    
-    <App />
-    
-  </React.StrictMode>,
-)
+if (rootElement) {
+  ReactDOM.render(
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>,
+    rootElement
+  );
+}
